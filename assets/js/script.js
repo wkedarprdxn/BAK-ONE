5bea552410dad292fe025d73cc71e57e018911ef
wkedarprdxn <kedarw@prdxn.com>
2020-10-22 22:41:11 +0530

    scroll up function in js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,18 @@ $(document).ready(function() {
             $('.filter').filter('.' + val).show(1000);
         }
     });
+    var btn = $('#scroll-up');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, '300');
+    });
 });
